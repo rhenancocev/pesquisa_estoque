992857cb1547b98027f4e39a37c99361e9f4ee36
Rhenan Cocev <rhenan_cocev01@hotmail.com>
2019-02-24 16:04:30 -0300

    teste

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const env = require('../.env');
+const env = require('./.env');
 
 const TelegramBot = require('node-telegram-bot-api');
 // replace the value below with the Telegram token you receive from @BotFather
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(env.token, { polling: true });
+
+
+bot.on ('text', (ctx) => {
+    const chatId = ctx.chat.id;
+
+    texto = ctx.text;
+    nome = ctx.from.first_name;
+    var comando = texto;
+
+    if (comando === '/start'){
+        bot.sendMessage(chatId, nome + ", você é gay");
+    }
+});
